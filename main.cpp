8b0b57e4f129ea0efd5fdb85abac2fe2f2857caf
Esteban Donis <estebandonis10@gmail.com>
2023-07-14 22:39:02 -0600

    Estrella

--- main.cpp
+++ main.cpp
@@ -9,24 +9,7 @@ Color currentColor(122, 122, 122);
 void render() {
     Framebuffer framebuffer(800, 600);
 
-    Color prueba(10, 10, 10);
-    clearColor = prueba;
-
     framebuffer.clear(clearColor);
-
-    Color NewCurrent(100,100,100);
-    currentColor = NewCurrent;
-
-    Vertex2 ver1 = {500, 500, Color(255, 0, 0)};
-    Vertex2 ver2 = {50, 50, Color(255, 0, 0)};
-
-    framebuffer.dibujarPunto(ver1, currentColor);
-    framebuffer.dibujarPunto(ver2, currentColor);
-
-    Vertex2 inicio = {100, 100, Color(255, 0, 0)};
-    Vertex2 fin = {200, 200, Color(255, 0, 0)};
-    Color verde(0, 255, 0);
-    framebuffer.dibujarLinea(inicio, fin, verde);
 
     // Definir un polígono con cuatro puntos
     std::vector<Point> puntos;
@@ -53,8 +36,8 @@ void render() {
     puntos.push_back(punto10);
 
     // Dibujar el polígono en el framebuffer con el color rojo
-    framebuffer.dibujarPoligono(puntos, Color(255, 0, 0));
-    framebuffer.fillPolygon(puntos, Color(255,255,255));
+    framebuffer.dibujarPoligono(puntos, Color(255, 255, 255));
+    framebuffer.fillPolygon(puntos, Color(255,255,0));
 
     framebuffer.renderBuffer();
 }
