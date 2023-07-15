1cb62a0c3ea7e45169e2a280122722e1e0509141
Esteban Donis <estebandonis10@gmail.com>
2023-07-15 17:06:19 -0600

    Cuadrado1

--- main.cpp
+++ main.cpp
@@ -15,20 +15,20 @@ void render() {
     currentColor = NewCurrent;
 
     // Definir un polígono con cuatro puntos
-    std::vector<Point> puntos;
-    Point punto1 = {321, 335};
-    Point punto2 = {288, 286};
-    Point punto3 = {339, 251};
-    Point punto4 = {374, 302};
+    std::vector<Point> puntos2;
+    Point punto1_2 = {321, 335};
+    Point punto2_2 = {288, 286};
+    Point punto3_2 = {339, 251};
+    Point punto4_2 = {374, 302};
 
-    puntos.push_back(punto1);
-    puntos.push_back(punto2);
-    puntos.push_back(punto3);
-    puntos.push_back(punto4);
+    puntos2.push_back(punto1_2);
+    puntos2.push_back(punto2_2);
+    puntos2.push_back(punto3_2);
+    puntos2.push_back(punto4_2);
 
     // Dibujar el polígono en el framebuffer con el color rojo
-    framebuffer.dibujarPoligono(puntos, Color(255, 255, 255));
-    framebuffer.fillPolygon(puntos, Color(0,255,0));
+    framebuffer.dibujarPoligono(puntos2, Color(255, 255, 255));
+    framebuffer.fillPolygon(puntos2, Color(0,255,0));
 
     framebuffer.renderBuffer();
 }
