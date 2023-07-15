3d33d1557ac38e18da3bf737d168b91292a7671a
Esteban Donis <estebandonis10@gmail.com>
2023-07-14 22:59:30 -0600

    Triangulo

--- main.cpp
+++ main.cpp
@@ -16,19 +16,17 @@ void render() {
 
     // Definir un polígono con cuatro puntos
     std::vector<Point> puntos;
-    Point punto1 = {321, 335};
-    Point punto2 = {288, 286};
-    Point punto3 = {339, 251};
-    Point punto4 = {374, 302};
+    Point punto1 = {377, 249};
+    Point punto2 = {411, 197};
+    Point punto3 = {436, 249};
 
     puntos.push_back(punto1);
     puntos.push_back(punto2);
     puntos.push_back(punto3);
-    puntos.push_back(punto4);
 
     // Dibujar el polígono en el framebuffer con el color rojo
     framebuffer.dibujarPoligono(puntos, Color(255, 255, 255));
-    framebuffer.fillPolygon(puntos, Color(0,255,0));
+    framebuffer.fillPolygon(puntos, Color(255,0,0));
 
     framebuffer.renderBuffer();
 }
